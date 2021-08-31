b9932996dea74cbb38855d8e995b656e3fa254ed
pythonchi <87922505+pythonchi@users.noreply.github.com>
2021-08-31 15:50:22 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import telebot
+from moviepy.editor import VideoFileClip
+import os
+
+bot = telebot.TeleBot('1972875235:AAFvDrQTPfviteeV-hVqs2CiDVXKDhnaXj0')
+
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+	bot.send_message(msg.chat.id,'Assalomu alaykum\n\nBot siz yuborgan videoni Gif sifatida yuboradi. Marhamat video yuboring!')
+
+@bot.message_handler(content_types=['video'])
+def convert(msg):
+	bot.send_message(msg.chat.id,'Kuting bu biroz vaqt olishi mumkin!')
+	try:
+		file_id = msg.video.file_id
+		file = bot.get_file(file_id)
+		file_path = file.file_path
+		s = bot.download_file(file_path)
+		with open(f'{msg.chat.id}.mp4', 'wb') as new_file:
+			new_file.write(s)
+		with VideoFileClip(f'{msg.chat.id}.mp4') as clip:
+			clip.write_gif(f"{msg.chat.id}.gif",fps=10)
+		with open(f"{msg.chat.id}.gif",'rb') as gif:
+			bot.send_animation(msg.chat.id,gif)
+		os.unlink(f'{msg.chat.id}.mp4')
+		os.unlink(f'{msg.chat.id}.gif')
+	except Exception as ex:
+		bot.send_message(msg.chat.id,'Xato... , Shartlarni to\'g\'ri bajaring!')
+		print(ex)
+	# file_id = msg.video.file_id
+	# file = bot.get_file(file_id)
+	# file_path = file.file_path
+	# s = bot.download_file(file_path)
+	# with open(f'{msg.chat.id}.mp4', 'wb') as new_file:
+	# 	new_file.write(s)
+bot.polling(none_stop=True)
